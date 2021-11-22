4ccc02414164e4baabf689b2491f027f28d3804f
rudyvee <71866640+rudyvee@users.noreply.github.com>
2021-11-21 19:22:42 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,102 +1,190 @@
 from client import client
+import json, time, traceback
 
 Client = client()
 loggedIn = False
-
-def options():
-    print('\n\tEnter "1 <host> <port>" to connect to a host')
-    print('\tEnter "2 <username> <password>" to login')
-    print('\tEnter "3" to view all available recipients')
-    print('\tEnter "4" to send a new message to a user')
-    print('\tEnter "5" to view all past message recipient')
-    print('\tEnter "6" to view message history between you and a recipient')
-    print('\tEnter "9" to diconnect\n')
-
-def connect(args):
-    argsArray = args.split()
-    # if len(argsArray) != 3:
-    #     print('Please provide a host and port number')
-    #     return
-    # newClient.connect(argsArray[1], int(argsArray[2]))
-    Client.connect('127.0.0.1', 12345)
-
-def login(args):
-    global loggedIn
-    argsArray = args.split()
-    # if len(argsArray) != 3:
-    #     print('Please provide a username and password')
-    #     return
-    # logginIn = newClient.login(argsArray[1], argsArray[2])
-    loggedIn = Client.login('jio', 'jio')
-
-def getGroups():
-    groups = Client.getGroups()
-    print('All past message recipients are ' + groups)
-
-def getUsers():
-    users = Client.getAvailableUsers()
-    print('Available users are: ' + users)
-
-def sendMessage(args):
-    argsArray = args.split()
-    if len(argsArray) < 2:
-        print('Please provide a user and message')
-        return
-    user = argsArray[1]
-    i = 2
-    message = []
-
-    while i < len(argsArray) - 1:
-        message.append(argsArray[i])
-
-    messageString = ''.join(message)
-    Client.sendMessage(user, messageString)
-
-def getMessageHistory(args):
-    argsArray = args.split()
-    if len(argsArray) != 2:
-        print('Please provide a recipient to view history')
-        return
-    user = argsArray[1]
-    message = Client.getMessageHistory(user, 1) # 1 as default iteration
-    print(message)
-
-def disconnect():
-    Client.disconnect()
+connected = False
 
 def main():
     print('Welcome to Priv, a secure messaging application')
     print('What would you like to do?')
 
     while True:
-        options()
-        args = input()
+        try:
+            options()
+            args = input('> ')
 
-        if args.startswith('1'):
-            connect(args)
-        elif args.startswith('2'):
-            login(args)
-        elif args.startswith('3'):
-            getUsers()
-        elif args.startswith('4'):
-            if loggedIn is False:
-                print('\tPlease login first')
-                continue
-            sendMessage(args)
-        elif args.startswith('5'):
-            if loggedIn is False:
-                print('\tPlease login first')
-                continue
-            getGroups()
-        elif args.startswith('6'):
-            if loggedIn is False:
-                print('\tPlease login first')
-                continue
-            getMessageHistory(args)
-        elif args.startswith('9'):
-            disconnect()
+            if args.startswith('9'):
+                disconnect()
+                print('Disconnected')
+                break
+
+            if not connected:
+                if args.startswith('1'):
+                    connect()
+                else:
+                    print('Please connect to the server first')
+            else:
+                if not loggedIn:
+                    if args.startswith('2'):
+                        login(args)
+                    else:
+                        print('Please login first')
+                else:
+                    if args.startswith('3'):
+                        getUsers()
+                    elif args.startswith('4'):
+                        getGroups()
+                    elif args.startswith('5'):
+                        sendMessage(args)
+                    elif args.startswith('6'):
+                        getMessageHistory(args)
+                    else:
+                        print('\tInvalid command')
+        except Exception as e:
+            if str(e) == 'timed out':
+                print('Request timed out')
+            elif str(e) == 'Decryption failed': # To prevent user from seeing where the error originated from in the stack
+                print('Decryption failed')
+            else:
+                traceback.print_exc()
+
+def options():
+    print('\n\tEnter "1" to connect to a host')
+    print('\tEnter "2 <username> <password>" to login')
+    print('\tEnter "3" to view all available recipients')
+    print('\tEnter "4" to view all past message recipient')
+    print('\tEnter "5 <recipient> <message>" to send a new message to a user')
+    print('\tEnter "6 <recipient>" to view message history between you and a recipient')
+    print('\tEnter "9" to diconnect and exit\n')
+
+def connect():
+    global connected
+    
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+
+    host = config['host']
+    port = config['port']
+
+    connected = Client.connect(host, port)
+
+def login(args):
+    global loggedIn
+    argsArray = args.split()
+
+    if len(argsArray) != 3:
+        print('Please provide a username and password')
+        return
+
+    loggedIn = Client.login(argsArray[1], argsArray[2])
+
+    if loggedIn:
+        print('Successfully logged in')
+    else:
+        print('Password is incorrect')
+
+def getGroups():
+    groups = Client.getGroups()
+    print('All past message recipients are:')
+
+    for group in groups:
+        print(f'  {group["recipient"]}')
+
+def getUsers():
+    users = Client.getAvailableUsers()
+    print('Available users are:')
+
+    for user in users:
+        print(f'  {user}')
+
+def sendMessage(args):
+    argsArray = args.split()
+
+    if len(argsArray) == 2:
+        print('Cannot send empty message')
+        return
+    elif len(argsArray) < 2:
+        print('Please provide a user and message')
+        return
+
+    user = argsArray[1]
+    i = 2
+    message = []
+
+    while i < len(argsArray):
+        if i < len(argsArray) - 1:
+            message.append(argsArray[i] + " ")
         else:
-            print('\tInvalid command')
+            message.append(argsArray[i])
+        i += 1
+
+    messageString = ''.join(message)
+    response = Client.sendMessage(user, messageString)
+
+    if response == '200':
+        print('Message successfully sent')
+    elif response == '403':
+        print('Cannot send messages to yourself')
+    elif response == '404':
+        print('Entered user does not exist')
+    elif response == '500':
+        print('Error encrypting message - recipient does not have a public key')
+    else:
+        print('Error sending message')
+
+def getMessageHistory(args):
+    argsArray = args.split()
+
+    if len(argsArray) != 2:
+        print('Please provide a recipient to view history')
+        return
+
+    user = argsArray[1]
+
+    recurseMessageHistory(user, 1)
+
+def recurseMessageHistory(user, iteration):
+    response = Client.getMessageHistory(user, iteration)
+
+    if response == 400:
+        print('No message history between you and requested recipient')
+        return
+    elif response == 403:
+        print('Cannot view message history with yourself')
+        return
+    elif response == 404:
+        print('Entered user does not exist')
+        return
+    elif response == 500:
+        print('User Private Key missing')
+        return
+
+    messages, end = response
+
+    print()
+
+    for message in messages:
+        print(f'{time.strftime("  %D %H:%M", time.localtime(message["sentAt"]))} | {message["sentBy"]}: {message["message"]}')
+
+    if end:
+        print('End of message history')
+    else:
+        while True:
+            print('-- Type <RET> for more, q to quit --')
+            keyInput = input()
+
+            if keyInput == '':
+                recurseMessageHistory(user, iteration + 1)
+                return
+            elif keyInput == 'q':
+                return
+            else:
+                continue
+
+def disconnect():
+    if connected:
+        Client.disconnect()
 
 if __name__=="__main__":
     main()
--- client.py
+++ client.py
@@ -1,8 +1,10 @@
-import socket
-import hashlib
+import socket, hashlib, json, rsa, time, os
+
+timeout = 0.5
 
 class client:
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.settimeout(5) # Number of seconds before timeout
 
     def connect(self, host, port):
         self.host = host
@@ -11,98 +13,163 @@ class client:
         try:
             self.connection.connect((self.host, self.port))
             print('Connected to: ' + self.host + ':' + str(self.port))
+            return True
         except socket.error as e:
             print(str(e))
+            return False
 
     def login(self, username, password):
         self.username = username
-        self.connection.send(str.encode(username))
-        self.connection.send(str.encode(hashlib.sha256(str.encode(password)).hexdigest()))
 
-        response = self.connection.recv(2048)
-        response = response.decode()
+        data = json.dumps({"username": username, "password": hashlib.sha256(str.encode(password)).hexdigest()})
+        self.connection.send(data.encode())
+        
+        loginResponse = self.receiveResponse()
 
-        if response == '200':
+        if loginResponse == '200':
             return True
+        elif loginResponse == '201':
+            try:
+                os.makedirs('./privateKeys/', exist_ok=False)
+            except:
+                pass
+
+            publicKey, privateKey = rsa.newkeys(512)
+            privateKeyPem = privateKey.save_pkcs1().decode('utf8')
+
+            f = open(f"./privateKeys/{self.username}PEM.pem", "w")
+            f.write(privateKeyPem)
+            f.close()
+
+            publicKeyPem = publicKey.save_pkcs1().decode('utf8')
+            data = json.dumps({"publicKey": publicKeyPem})
+            self.connection.send(data.encode())
+            
+            newResponse = self.receiveResponse()
+
+            if newResponse == '200':
+                return True
+            else:
+                return False
         else:
             return False
 
-    def getGroups(self):
-        self.connection.send(str.encode('1'))
-        self.receiveAck()
-
-        groups = {}
-
-        while True:
-            key = self.connection.recv(2048)
-            key = key.decode()
-
-            if key == 'end':
-                break
-                
-            receiver = self.connection.recv(2048)
-            receiver = receiver.decode()
-
-            groups[key] = receiver
-        
-        return groups
-
-    def getMessageHistory(self, user, iteration): # Need to implement iteration
-        self.connection.send(str.encode('2'))
-        self.receiveAck()
-
-        self.connection.send(str.encode(user))
-        self.connection.send(str.encode(iteration))
-
-        messages = []
-
-        while True:
-            sentAt = self.connection.recv(2048)
-            sentBy = sentBy.decode()
-
-            if sentAt == 'end':
-                break
-
-            sentBy = self.connection.recv(2048)
-            messageText = self.connection.recv(2048)
-            
-            sentAt = sentAt.decode()
-            messageText = messageText.decode()
-
-            messages.append({'sentAt': sentAt, 'sentBy': sentBy, 'messageText': messageText})
-
-        return messages
-
-    def sendMessage(self, user, message): # implement either ecc or rsa
+    def getAvailableUsers(self):
         self.connection.send(str.encode('3'))
         self.receiveAck()
 
-        self.connection.send(str.encode(user))
-        self.connection.send(str.encode(message))
-    
-    def getAvailableUsers(self):
-        self.connection.send(str.encode('4'))
-        self.receiveAck()
-
-        availableUsersArray = []
-
-        while True:
-            user = self.connection.recv(2048)
-            user = user.decode()
-
-            if user == 'end':
-                break
-
-            availableUsersArray.append(user)
+        data = self.receiveResponse()
+        data = json.loads(data)
+        availableUsersArray = data.get("recipients")
 
         return availableUsersArray
 
-    def disconnect(self):
+    def getGroups(self):
+        self.connection.send(str.encode('4'))
+        self.receiveAck()
+
+        data = self.receiveResponse()
+        data = json.loads(data)
+        groups = data.get("groups")
+
+        return groups
+
+    def sendMessage(self, recipient, message):
         self.connection.send(str.encode('5'))
+        self.receiveAck()
+
+        data = json.dumps({"recipient": recipient})
+        self.connection.send(data.encode())
+
+        data = self.receiveResponse()
+
+        if data == '404':
+            return '404'
+        elif data == '403':
+            return '403'
+
+        data = json.loads(data)
+        publicKey = data.get("publicKey")
+
+        publicKey = rsa.PublicKey.load_pkcs1(publicKey)
+        encrypted = rsa.encrypt(message.encode('utf-8'), publicKey)
+
+        self.connection.send(encrypted)
+
+        return self.receiveResponse()
+
+    def getMessageHistory(self, recipient, iteration):
+        self.connection.send(str.encode('6'))
+        self.receiveAck()
+
+        data = json.dumps({"recipient": recipient, "iteration": iteration})
+        self.connection.send(data.encode())
+
+        data = self.receiveFullResponse()
+
+        if data == '400':
+            return 400
+        elif data == '403':
+            return 403
+        elif data == '404':
+            return 404
+
+        data = json.loads(data)
+        messages = data.get("messages")
+        decryptedArray = []
+
+        try:
+            with open(f'./privateKeys/{self.username}PEM.pem', mode='rb') as privateKeyFile:
+                keydata = privateKeyFile.read()
+        except:
+            return 500
+
+        privateKey = rsa.PrivateKey.load_pkcs1(keydata)
+
+        for message in messages:
+            message = json.loads(message)
+            if message['sentBy'] != self.username:
+                decrypted = str(rsa.decrypt(message['message'], privateKey))
+                message['message'] = decrypted[2:-1]
+            else:
+                message['message'] = '=== Message Encrypted ==='
+
+            decryptedArray.append(message)
+
+        end = data.get("end")
+
+        return decryptedArray, end
+
+    def disconnect(self):
+        self.connection.send(str.encode('9'))
         self.connection.close()
 
     def receiveAck(self):
-        ack = self.connection.recv(2048)
-        ack = ack.decode()
+        ack = self.receiveResponse()
         if ack != '1':
             raise Exception('Ack not received')
+
+    def receiveResponse(self):
+        response = self.connection.recv(2048)
+        return response.decode()
+
+    def receiveFullResponse(self):
+        self.connection.setblocking(0)
+        total_data = [];
+        data = '';
+        begin = time.time()
+
+        while 1:
+            if total_data and time.time() - begin > timeout:
+                break
+            
+            try:
+                data = self.connection.recv(4096)
+                if data:
+                    total_data.append(data)
+                    begin = time.time()
+            except:
+                pass
+        
+        self.connection.setblocking(1)
+        return b''.join(total_data).decode()
